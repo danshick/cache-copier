b95d819f8c5ad851612eaae5e435a329c456ff1b
Dan Shick <dan.shick@gmail.com>
2014-01-17 18:07:19 -0500

    First try. Identifies new files that might potentially be mp4s.

---
+++ app.js
@@ -0,0 +1,34 @@
+var fs = require('fs');
+var Buffer = require('buffer').Buffer;
+var watchr = require('watchr');
+
+var cachedir = process.argv[2];
+
+watchr.watch({
+  paths: [cachedir],
+  listeners: {
+    error: function(err){
+            console.log('A watchr error occured: ', err);
+          },
+    change: function(changeType,filePath,fileCurrentStat,filePreviousStat){
+        if( changeType == "" ){
+          //possibly create entry for file now
+        }
+        else if( changeType == "create" ){
+          console.log(filePath);
+          fs.open(filePath, 'r', function(err, fd){
+            if (err) return err;
+            var buffer = new Buffer(8);
+            fs.read(fd, buffer, 0, 8, 0, function(err, num){
+              var ident = buffer.toString('hex', 0, num);
+              console.log("Eight bytes " + ident );
+              if(ident.match("000000[0-9]{2}66747970")){
+                console.log("BIG WIN!!!");
+              }
+            });
+          });
+        }
+      console.log('a change event occured:',arguments);
+    }
+  }
+});
